256b75f2d69b96f2d41864f824e6161ee5bc3c12
Vinecreeper888 <siddhanth8@gmail.com>
2020-06-06 22:01:38 +0530

    counter added

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Counter extends Component {
+   componentDidUpdate(prevProps, prevState) {
+       //after a component is updated
+       console.log('prevProps',prevProps);
+       console.log('prevState',prevState);
+       if(prevProps.counter.value != this.props.counter.value) {
+
+       }
+
+   }
+
+   componentWillUnmount() {
+       console.log("Counter - Unmount");
+   }
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+    render() { 
+        console.log("Counter - Rendered"); 
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={() => this.props.onIncrement(this.props.counter)} 
+                    className="btn btn-secondary btn-sm">Increment</button>
+                <button 
+                    onClick={() => this.props.onDelete(this.props.counter.id)} 
+                    className="btn btn-danger btn-sm m-2">Delete</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const {value} = this.props.counter;
+        return value === 0 ? 'Zero' : value;
+    }
+}
+ 
+export default Counter;
